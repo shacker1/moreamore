13b9d8d5fafee722284971a662bcdcdbd32d8859
Sylvia Hacker <shacker@technicalpursuit.com>
2011-06-23 13:48:48 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moreamore::Application.config.secret_token = '580e0bc15d030425c67e64fa7d5cb4e4cb77778e7fdfa912f14c7fb5bf4c27cd12017fbe9a18f0273f409152e7240bc1958b007774f1b54a82697c0d8df5e8c9'
