c56cb829ad2f2f1b414ff8195f65e4e45adf8208
Sylvia Hacker <shacker@technicalpursuit.com>
2011-07-06 08:51:35 -0600

    Chapt 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 module ApplicationHelper
+  def logo
+ logo = image_tag("MP900448358.JPG", :alt => "Image Tag", :class => "round")   
+  end
  # return a title on a per page basis
   def title
    base_title = "Moreamore Delicious Foods"
